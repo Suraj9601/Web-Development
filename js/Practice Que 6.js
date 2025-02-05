b64a12cde603ca617d9fbd5e2e86328d1608be72
Suraj Aglave <aglavesuraj96@gmail.com>
2025-02-05 21:37:23 +0530

    Create Practice Que 6.js

---
+++ js/Practice Que 6.js
@@ -0,0 +1,15 @@
+let light = "red";
+
+switch(light) {
+    case "red":
+        console.log("Stop!")
+        break;
+    case "yellow":
+        console.log("Look!")
+        break;
+    case "green":
+        console.log("Go")
+        break;
+    default:
+        console.log("Invalid color")
+}
