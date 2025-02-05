8984eaa43d189aabb104826ddbf574f7867482cf
Suraj Aglave <aglavesuraj96@gmail.com>
2025-02-05 21:37:35 +0530

    Create Practice  Que 8.js

---
+++ js/Practice  Que 8.js
@@ -0,0 +1,11 @@
+let msg ="help!";
+newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
+
+let name2 = "Apna college";
+let slice_name2 = name2.slice(4,9);
+let index_name2 = name2.indexOf("na");
+let replace_name2 = name2.replace("Apna","our")
+console.log(slice_name2);
+console.log(index_name2);
+console.log(replace_name2);
