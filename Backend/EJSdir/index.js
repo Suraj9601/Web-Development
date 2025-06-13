2c639b858bebe103e1d7fefff4593d44de359e44
Suraj Aglave <aglavesuraj96@gmail.com>
2025-06-13 18:37:56 +0530

    Change

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -6,22 +6,44 @@ let port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({extended : true}));
 
 app.get("/", (req, res) => {
     res.render("Home.ejs");
-})
-app.get("/ig/:username", (req, res) => {
+});
+
+
+app.get("/ig/:username", (req,res) => {
+    console.log(req.params);
+    let {username} = req.params;
+    res.render("insta-username.ejs",{username});
+});
+
+
+
+app.get("/ig/profile/:username", (req, res) => {
     let {username} = req.params;
     const instaData = require("./data.json");
     const data = instaData[username];
     console.log(data);
     if(data) {
-        res.render("instagram.ejs", {data});
+        res.render("insta-profile.ejs", {data});
     } else {
         res.render("error.ejs");
     }
     
-})
+});
+
+app.get("/register", (req, res) => {
+    let {username, password} = req.query;
+    console.log({username, password});
+    res.send(`This is standerd response for GET request. Welcome ${username}`);
+});
+app.post("/register", (req, res) => {
+    let {username, password} = req.body;
+    console.log({username, password});
+    res.send(`This is standerd response for POST request. Welcome ${username}`);
+});
 
 app.listen(port, () => {
     console.log("listening port on 8080");
---
+++ Backend/REST_APIs/index.js
@@ -0,0 +1,88 @@
+const express =require("express");
+const app = express();
+let port = 8080;
+const path = require("path");
+const {v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        id : uuidv4(),
+        username : "Suraj_Aglave_96",
+        content : "Choose a Good Heart not a Face."
+    },
+    {
+        id :uuidv4(),
+        username: "Smit_K_001",
+        content : "Choose a Good Heart not a Face."
+    },
+    {
+        id : uuidv4(),
+        username : "Ketan_patil",
+        content : "Choose a Good Heart not a Face."
+    }
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+});
+
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/post", (req, res) => {
+    console.log(req.body);
+    let id = uuidv4();
+    let {username, content} = req.body;
+    posts.push({id, username,content});
+    res.redirect("/posts");
+});
+
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post});
+});
+
+
+app.patch("/posts/:id", (req, res)=> {
+    let {id} = req.params;
+    console.log(id);
+    let newContent = req.body.content;
+    console.log(newContent);
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
+
+
+app.listen(port, () => {
+    console.log(`Listning on port ${port}`);
+});
+
+
+
